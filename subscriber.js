d26c35af55cd046eb0eadb67289e8ddb0990c265
Cameron Howley <howlec2@rpi.edu>
2022-11-08 23:18:50 -0500

    example subscriber connects to EMQX Cloud broker

---
+++ subscriber.js
@@ -0,0 +1,28 @@
+// This file creates an MQTT client and connects to the EMQX Cloud
+// Example of subscribing to a topic and sending/receiving messages
+
+const mqtt = require('mqtt')
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+  // Auth
+  clientId: 'emqx_test',
+  username: 'emqx_test',
+  password: 'emqx_test',
+}
+const client  = mqtt.connect('mqtt://broker.emqx.io:1883', options)
+client.on('connect', function () {
+  console.log('Connected')
+  client.subscribe('test', function (err) {
+    if (!err) {
+      client.publish('test', 'Hello mqtt')
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
